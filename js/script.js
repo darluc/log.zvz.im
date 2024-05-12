142f79cd4057758bf83d54dad7df9b6cbc2fea26
darluc <darluc@users.noreply.github.com>
2024-05-12 13:16:26 +0000

    Deploying to gh-pages

---
+++ js/script.js
@@ -0,0 +1,65 @@
+(function($){
+  /* because of having no sharing privileges of wechat, comment these lines of code.
+  requirejs(['//res.wx.qq.com/open/js/jweixin-1.2.0.js'], function(wx){
+    $.get('//zvz.im/blog/jssdk', function(data){
+      var cfg = $.extends({}, data, {
+        'jsApiList': []
+      });
+      delete cfg.url;
+      wx.config(cfg);
+    });
+    wx.ready(function(){
+    });
+  });
+  */
+  
+  // Caption
+  $('.article-entry').each(function(i){
+    $(this).find('img').each(function(){
+      if ($(this).parent().hasClass('fancybox')) return;
+
+      var alt = this.alt;
+
+      if (alt) $(this).after('<span class="caption">' + alt + '</span>');
+
+      $(this).wrap('<a href="' + this.src + '" title="' + alt + '" class="fancybox"></a>');
+    });
+
+    $(this).find('.fancybox').each(function(){
+      $(this).attr('rel', 'article' + i);
+    });
+  });
+
+  if ($.fancybox){
+    $('.fancybox').fancybox();
+  }
+
+  // Mobile nav
+  var $container = $('#container'),
+    isMobileNavAnim = false,
+    mobileNavAnimDuration = 200;
+
+  var startMobileNavAnim = function(){
+    isMobileNavAnim = true;
+  };
+
+  var stopMobileNavAnim = function(){
+    setTimeout(function(){
+      isMobileNavAnim = false;
+    }, mobileNavAnimDuration);
+  }
+
+  $('#main-nav-toggle').on('click', function(){
+    if (isMobileNavAnim) return;
+
+    startMobileNavAnim();
+    $container.toggleClass('mobile-nav-on');
+    stopMobileNavAnim();
+  });
+
+  $('#wrap').on('click', function(){
+    if (isMobileNavAnim || !$container.hasClass('mobile-nav-on')) return;
+
+    $container.removeClass('mobile-nav-on');
+  });
+})(jQuery);
